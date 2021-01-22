c235befb68351879ed0e1f3f34c4c7394123d520
NatBNW <70111199+NatBNW@users.noreply.github.com>
2021-01-22 15:50:13 -0800

    Initial design detail page

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,12 +1,25 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
 
 export default function DetailsScreen({navigation}) {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>DETAILS</Text>
             <View style={styles.separator} />
-            <Button onPress={() => navigation.goBack()} title="Go back home" />
+            <Text style={styles.title}>Name of This Restroom</Text>
+            <View style={styles.separator} />
+            <Image
+        style={{
+            resizeMode: "cover",
+            height: 100,
+            width: 200
+        }}
+        source={require('../assets/placeholder-image.png')}
+        />
+        <View style={styles.separator} />
+        <Text style={styles.base}>Description of this restroom</Text>
+        <View style={styles.separator} />
+        <Button onPress={() => navigation.goBack()} title="Go back home" />
         </View>
     );
 }
@@ -27,3 +40,4 @@ const styles = StyleSheet.create({
         width: '80%',
     },
 });
+
