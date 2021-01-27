6f7672d1a230ce1c8e257ed88f8a1946ac71c720
NinjaDuck9000 <mvroom3@gmail.com>
2021-01-27 12:42:51 -0800

    adding menu button

--- navigation/Menu.js
+++ navigation/Menu.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Button, View, Text, Dimensions } from 'react-native';
+import { StyleSheet, Button, View, Text, Dimensions } from 'react-native';
 import { createDrawerNavigator, DrawerContent, DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
+import MenuIcon from "react-native-bootstrap-icons/icons/three-dots";
 import MapScreen from '../screens/MapScreen';
 import AddBathroomScreen from '../screens/AddBathroomScreen';
 import AboutScreen from '../screens/AboutScreen';
@@ -59,7 +60,7 @@ const Drawer = createDrawerNavigator();
 
 export default function Menu() {
     return (
-        <Drawer.Navigator initialRouteName="Home" drawerContent={props => <MenuContent {...props}/>}>
+        <Drawer.Navigator initialRouteName="Home" drawerContent={props => <MenuContent {...props} />}>
             <Drawer.Screen name="Home" component={HomeScreen} />
             <Drawer.Screen name="Map" component={MapScreen} />
             <Drawer.Screen name="Add A Bathroom" component={AddBathroomScreen} />
@@ -74,11 +75,24 @@ export default function Menu() {
     );
 }
 
-function MenuContent({...props}) {
+function MenuContent({ navigation, ...props }) {
     return (
         <DrawerContentScrollView {...props}>
             <DrawerItemList {...props} />
             <DrawerItem label="TEST"></DrawerItem>
+            <View style={styles.menuButton} >
+                <MenuIcon onPress={() => navigation.toggleDrawer()} width="20" height="20" fill="rgb(142, 142, 142)" />
+            </View>
         </DrawerContentScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    menuButton: {
+        position: 'absolute',
+        marginTop: 35,
+        marginLeft: Dimensions.get('window').width * .65, // fix later
+        padding: 10,
+        backgroundColor: '#FFFFFF',
+    },
+});
